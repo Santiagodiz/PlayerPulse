91cf1204486f6dc23d9fc78429a44438f1e3b67a
DavidPrats <david.prats.vargas@alumnos.upm.es>
2024-09-30 02:04:11 +0200

    news scrapper

---
+++ Data_extraction/news_scrapper.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+# Creamos un DataFrame vacío
+df = pd.DataFrame(columns=['title', 'date', 'content'])
+
+# Iteramos sobre las páginas de noticias
+for i in range(1, 376):
+    url = f'https://www.fichajes.com/actualidad/{i}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Encuentra todos los articulos de la pagina
+    news = soup.find_all('article')
+    for new in news:
+        # Recoge el metadata de cada articulo
+        title = new.find('h3').text
+        date = new.find('span').text
+
+        # Entra en el articulo para recoger el contenido
+        url = new.find('a')['href']
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            content1 = soup.find('h2', class_='articleLead').contents[0]
+        except:
+            content1 = ''
+        try: 
+            content2 = soup.find('h2', class_='articleLead').contents[1].text
+        except:
+            content2 = ''
+        content = content1 + ' ' + content2
+        df = pd.concat([df, pd.DataFrame({'title': [title], 'date': [date], 'content': [content]})])
+
+    print(f'Page {i} scraped')
+
+# Guardamos el DataFrame en un archivo CSV
+df.to_csv('Data/news.csv', index=False)
